576f8929b2eca55ca78cd14599ec6040b87ec6fa
Yama <82410341+thelkotolsantosh@users.noreply.github.com>
2026-05-09 05:21:51 +0530

    Added visualization for login status count

--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+logs = pd.read_csv("data/sample_logs.csv")
+
+logs['status'].value_counts().plot(kind='bar')
+plt.title("Login Status Count")
+plt.show()
